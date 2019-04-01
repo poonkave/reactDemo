9307b8408f7b4be244d8a4ad4944757ec8875938
John Chacko <poonkave@gmail.com>
2019-04-01 10:45:15 +0100

    Update quantity.js

--- cart/components/quantity.js
+++ cart/components/quantity.js
@@ -1,4 +1,4 @@
-﻿"use strict";
+"use strict";
 
 var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
@@ -38,7 +38,7 @@ var Quantity = (function (_React$Component) {
         key: "setQty",
         value: function setQty(evt) {
             this.state.qty = evt.target.value;
-            this.props.updateQuantity(this.props.id, evt.target.value);
+            this.props.onUpdateQty(this.props.id, evt.target.value);
         }
     }, {
         key: "render",
