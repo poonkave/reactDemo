e9fe23642dfc79250c04cde97904fd19d75f26fd
John Chacko <poonkave@gmail.com>
2019-04-01 10:44:39 +0100

    Update items.js

--- cart/components/items.js
+++ cart/components/items.js
@@ -1,4 +1,4 @@
-﻿"use strict";
+"use strict";
 
 var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
@@ -27,8 +27,8 @@ var Items = (function (_React$Component) {
                 { className: "list-group" },
                 this.props.items.map(function (item) {
                     return React.createElement(Item, { key: item.id, item: item,
-                        removeItem: _this.props.removeItem,
-                        updateQuantity: _this.props.updateQuantity });
+                        onRemoveItem: _this.props.onRemoveItem,
+                        onUpdateQty: _this.props.onUpdateQty });
                 })
             );
         }
