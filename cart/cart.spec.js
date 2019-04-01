be58996557a4dd9949c72ef012fb6ef2cf66ffea
John Chacko <poonkave@gmail.com>
2019-04-01 09:32:30 +0100

    Add files via upload

---
+++ cart/cart.spec.js
@@ -0,0 +1,19 @@
+﻿"use strict";
+
+var state = {
+    counter: 0
+};
+
+describe("BetSlipCounter", function () {
+    it("renders without crashing", function () {
+        var div = document.createElement("div");
+        ReactDOM.render(React.createElement(BetSlipCounter, state), div);
+    });
+
+    it("renders and matches our snapshot", function () {
+        var component = renderer.create(React.createElement(BetSlipCounter, state));
+        var tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+});
+
---
+++ cart/cart.spec.jsx
@@ -0,0 +1,22 @@
+﻿
+ 
+const state = {
+    counter: 0
+};
+
+describe("BetSlipCounter", () => {
+    it("renders without crashing", () => {
+        const div = document.createElement("div");
+        ReactDOM.render(<BetSlipCounter {...state} />, div);
+    });
+
+
+    it("renders and matches our snapshot", () => {
+        const component = renderer.create(
+            <BetSlipCounter {...state}  />
+        );
+        const tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
+});
